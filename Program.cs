a8c16c386647f6041a095613d5b1b4b2c9fd9623
Kulakov Anton <kulakov_3000@mail.ru>
2022-09-05 20:23:42 +0300

    Домашняя работа 5

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿
-#nullable disable
-
-//Задача 35
-
+//Задача 34
 // int[] FillArray(int length)
 // {
 //     int[] array = new int[length];
@@ -10,36 +7,36 @@
 //     Random random = new Random();
 //     for (int i = 0; i < length; i++)
 //     {
-//         array[i] = random.Next(0, 200);
+//         array[i] = random.Next(100, 1000);
 //     }
 //     return array;
 // }
 
-// // void PrintArray(int[] array)
-// // {
-// //     if(array.Length == 0)
-// //     {
-// //         Console.WriteLine("Что-то пошло не так");
-// //         return;
-// //     }
-// //     Console.Write("[");
+// void PrintArray(int[] array)
+// {
+//     if(array.Length == 0)
+//     {
+//         Console.WriteLine("Что-то пошло не так");
+//         return;
+//     }
+//     Console.Write("[");
 
-// //     for (int i = 0; i < array.Length - 1; i++)
-// //     {
-// //         Console.Write($"{array[i]},");
-// //     }
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write($"{array[i]},");
+//     }
 
-// //     Console.Write(array[array.Length - 1]);
-// //     Console.Write("]");
-// // }
+//     Console.Write(array[array.Length - 1]);
+//     Console.Write("]");
+// }
 
-// int SumArr(int[] array)
+// int CheckArray(int[] array)
 // {
     
 //     int result = 0;
 //     for (int i = 0; i < array.Length; i++)
 //     {
-//         if(array[i] > 9 && array[i] < 100)
+//         if(array[i] % 2 == 0)
 //         {
 //             result++;
 //         }
@@ -48,8 +45,8 @@
 // }
 
 
-// //Console.WriteLine("Enter length - ");
-// //bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+// // Console.Write("Размер массива: ");
+// // bool isNumber = int.TryParse(Console.ReadLine(), out int length);
 
 // // if (!isNumber || length <= 0)
 // // {
@@ -57,23 +54,86 @@
 // //     return;
 // // }
 
-// int length = 123;
+// int length = 12;
 
 // int[] array = FillArray(length);
-// //PrintArray(array);
+// PrintArray(array);
 
-// int result = SumArr(array);
-
+// int result = CheckArray(array);
+// Console.WriteLine();
 // Console.Write($"Количество элементов: {result}");
 
 
 
 
+//Задача 36
 
 
+// int[] FillArray(int length)
+// {
+//     int[] array = new int[length];
+    
+//     Random random = new Random();
+//     for (int i = 0; i < length; i++)
+//     {
+//         array[i] = random.Next(0, 11);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     if(array.Length == 0)
+//     {
+//         Console.WriteLine("Что-то пошло не так");
+//         return;
+//     }
+//     Console.Write("[");
+
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write($"{array[i]},");
+//     }
+
+//     Console.Write(array[array.Length - 1]);
+//     Console.Write("]");
+// }
+
+// int SumArray(int[] array)
+// {
+    
+//     int result = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(i % 2 == 1)
+//         {
+//             result = result + array[i];
+//         }
+//     }
+//     return result;
+// }
 
 
+// // Console.Write("Размер массива: ");
+// // bool isNumber = int.TryParse(Console.ReadLine(), out int length);
 
+// // if (!isNumber || length <= 0)
+// // {
+// //     Console.WriteLine("Invalid number");
+// //     return;
+// // }
+
+// int length = 4;
+
+// int[] array = FillArray(length);
+// PrintArray(array);
+
+// int result = SumArray(array);
+
+// Console.Write($"Сумма элементов: {result}");
+
+
+//Задача 38
 
 
 
@@ -84,7 +144,7 @@ int[] FillArray(int length)
     Random random = new Random();
     for (int i = 0; i < length; i++)
     {
-        array[i] = random.Next(-10, 10);
+        array[i] = random.Next(1, 10);
     }
     return array;
 }
@@ -107,32 +167,37 @@ void PrintArray(int[] array)
     Console.Write("]");
 }
 
-int[] SumArr(int[] array)
+int GetMax(int[] array)
 {
-    int[] sums;
-
-    if(array.Length % 2 == 1)
-    {
-        sums = new int[array.Length / 2 + 1];
-    }
-    else{
-        sums = new int[array.Length / 2];
-    }
     
-
-    for (int i = 0; i < array.Length / 2 + 1; i++)
+    int result = array[0];
+    for (int i = 1; i < array.Length; i++)
     {
-        if(i != (array.Length - 1 - i))
+        if(array[i] > result)
         {
-            sums[i] = array[i] * array[array.Length - 1 - i];
+            result = array[i];
         }
     }
-    return sums;
+    return result;
+}
+
+int GetMin(int[] array)
+{
+    
+    int result = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if(array[i] < result)
+        {
+            result = array[i];
+        }
+    }
+    return result;
 }
 
 
-//Console.WriteLine("Enter length - ");
-//bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+// Console.Write("Размер массива: ");
+// bool isNumber = int.TryParse(Console.ReadLine(), out int length);
 
 // if (!isNumber || length <= 0)
 // {
@@ -140,113 +205,12 @@ int[] SumArr(int[] array)
 //     return;
 // }
 
-int length = 5;
+int length = 4;
 
 int[] array = FillArray(length);
 PrintArray(array);
-Console.WriteLine();
-PrintArray(SumArr(array));
 
+int min = GetMin(array);
+int max = GetMax(array);
 
-//Console.Write($"Количество элементов: {result}");
-
-
-
-
-
-
-
-
-
-
-
-
-//Задайте массив из 12 элементов, заполненный случайными числами из
-// промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
-//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
-
-// Console.WriteLine("Enter length - ");
-
-
-
-// bool isNumber = int.TryParse(Console.ReadLine(), out int length);
-// if (!isNumber || length <= 0)
-// {
-//     Console.WriteLine("Invalid number");
-//     return;
-// }
-
-// int[] array = FillArray(length);
-// PrintArray(array);
-
-// int sumPositiveNumber = GetSumPositiveNumberInArray(array);
-
-// Console.WriteLine($"sum Positive Number - {sumPositiveNumber}");
-
-// int sumNegativeNumber = GetSumNegativeNumberInArray(array);
-
-// Console.WriteLine($"sum negative Number - {sumNegativeNumber}");
-
-// int GetSumNegativeNumberInArray(int[] array)
-// {
-//     int sumNegativeNumber = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if(array[i] < 0)
-//         {
-//             sumNegativeNumber += array[i];
-//         }
-//     }
-
-//     return sumNegativeNumber;
-// }
-
-
-
-// int GetSumPositiveNumberInArray(int[] array)
-// {
-//     int sumPositiveNumber = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if(array[i] >= 0)
-//         {
-//          sumPositiveNumber += array[i];
-//         }
-//     }
-
-//     return sumPositiveNumber;
-// }
-
-
-
-
-
-// void PrintArray(int[] array)
-// {
-//     if(array.Length == 0)
-//     {
-//         Console.WriteLine("Что-то пошло не так");
-//         return;
-//     }
-//     Console.Write("[");
-
-//     for (int i = 0; i < array.Length - 1; i++)
-//     {
-//         Console.Write($"{array[i]},");
-//     }
-
-//     Console.Write(array[array.Length - 1]);
-//     Console.Write("]");
-// }
-
-// int[] FillArray(int length)
-// {
-//     int[] array = new int[length];
-    
-//     Random random = new Random();
-//     for (int i = 0; i < length; i++)
-//     {
-//         array[i] = random.Next(-9, 10);
-//     }
-//     return array;
-// }
+Console.Write($"Разность между максимальным и минимальным элементами: {max - min}");
